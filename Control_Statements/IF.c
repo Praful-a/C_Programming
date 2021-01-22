636eb2ffe0e5cb20dabd36ff59dfd727fb7669c9
praful-cyber <mca20017@glbitm.ac.in>
2021-01-22 21:56:55 +0530

    add in if file find greatest number program.

--- Control_Statements/IF.c
+++ Control_Statements/IF.c
@@ -4,7 +4,8 @@ Syntax:
 if(expression)
 	//code to be executed
 */
-
+/* Find the number is even or not  */
+/*
 #include<stdio.h>
 int main()
 {
@@ -16,4 +17,29 @@ int main()
 		printf("%d is even number", number);
 	}
 	return 0;
+}			*/
+
+/* Program to find the largest number of the three.  */
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("Enter three numbers: ");
+	scanf("%d %d %d", &a, &b, &c);
+	if(a>b && a>c)
+	{
+		printf("%d is largest", a);
+	}
+	if(b>a && b>c)
+	{
+		printf("%d is largest", b);
+	}
+	if (c>a && c>b)
+	{
+		printf("%d is largest", c);
+	}
+	if (a==b && a==c)
+	{
+		printf("All are equal");
+	}
 }
