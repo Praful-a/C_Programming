dd39a88c4c5d76f7750af249c699a2ac862e29a5
praful-cyber <mca20017@glbitm.ac.in>
2021-04-16 14:31:23 +0530

    solved reverse array problem.

--- Hacker_rank/reverse.c
+++ Hacker_rank/reverse.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	int size, i, arr[100];
-	scanf("%d", &size);
-	// int arr[size];
-	for(i=0;i<size;i++)
+	int num, i, *arr;
+	scanf("%d", &num);
+	arr = (int*) malloc(num * sizeof(int));
+	for(i=0; i<num; i++)
 	{
-		scanf("%d", &arr[i]);
+		scanf("%d", arr + i);
 	}
-	for(i=size;i>0;i--)
+	for(i=num-1; i>=0; i--)
 	{
-		printf("%d", arr[i]);	
+		printf("%d ", *(arr + i));	
 	}
 	return 0;
 }
